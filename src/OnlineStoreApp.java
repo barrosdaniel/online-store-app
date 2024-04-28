dc54d12ff73b5e5e8aacda2fcd27a581dd38d319
Daniel Barros <daniel.barros@cqumail.com>
2024-04-28 20:24:34 +0800

    Add application menu.

--- src/OnlineStoreApp.java
+++ src/OnlineStoreApp.java
@@ -1,5 +1,17 @@
 public class OnlineStoreApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the project.");
+        printHeader();
+    }
+
+    private static void printHeader() {
+        System.out.println("==================================================");
+        System.out.println("ONLINE STORE APP");
+        System.out.println("==================================================");
+        System.out.println("MAIN MENU");
+        System.out.println("Select from the following options:");
+        System.out.println("1. Purchase Book");
+        System.out.println("2. Purchase Movie");
+        System.out.println("3. Exit application");
+        System.out.println("*****");
     }
 }
---
+++ src/TaskI.java
@@ -0,0 +1,4 @@
+public interface TaskI {
+    public void executeTask();
+    public void getResult();
+}

d84658dfa1faf53e677afc0e78d941485d7c2854
Daniel Barros <daniel.barros@cqumail.com>
2024-04-28 20:55:24 +0800

    Add user input handling and validation.

--- src/OnlineStoreApp.java
+++ src/OnlineStoreApp.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class OnlineStoreApp {
+    private static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        printHeader();
+        while (true) {
+            printHeader();
+            int userOption = getUserOption();
+            if (userOption == 3) {
+                System.out.println("Thank you for using the Online Store App. "
+                        + "Goodbye.");
+                break;
+            } else {
+                System.out.println("Selected option: " + userOption);
+            }
+        }
     }
 
     private static void printHeader() {
@@ -13,5 +27,28 @@ public class OnlineStoreApp {
         System.out.println("2. Purchase Movie");
         System.out.println("3. Exit application");
         System.out.println("*****");
+    }
+
+    private static int getUserOption() {
+        int userOption;
+        while (true) {
+            System.out.print("Enter your option > ");
+            if (scanner.hasNextInt()) {
+                userOption = scanner.nextInt();
+                if (userOption >= 1 && userOption <= 3) {
+                    break;
+                } else {
+                    System.out.println("ERROR: Invalid option entered. Try "
+                            + "again.\n");
+                }
+            } else {
+                System.out.println("ERROR: Invalid option entered. Try again."
+                        + "\n");
+                scanner.next(); // This is required to remove the but where an 
+                                // invalid option stays is kept in the scanner 
+                                // buffer.
+            }
+        }
+        return userOption;
     }
 }
